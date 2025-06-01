ffc871558d6cc24ded0b877a3696bb2183651941
David Holland <info@dustvoice.de>
2025-06-01 16:26:51 +0200

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "DustBrain",
+    pageTitle: "Brain",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
